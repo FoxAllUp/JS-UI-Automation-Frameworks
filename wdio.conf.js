f435db17506b98cf6697ceeb8151964b1de5f5b2
FoxAllUp <richardhanzel93@gmail.com>
2025-07-06 19:25:06 +0200

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -162,16 +162,25 @@ exports.config = {
   // The only one supported by default is 'dot'
   // see also: https://webdriver.io/docs/dot-reporter
   reporters: [
-    "spec",
-    [
-      "allure",
-      {
-        outputDir: "allure-results",
-        disableWebdriverStepsReporting: true,
-        disableWebdriverScreenshotsReporting: true,
-      },
-    ],
+  "spec",
+  [
+    "allure",
+    {
+      outputDir: "allure-results",
+      disableWebdriverStepsReporting: true,
+      disableWebdriverScreenshotsReporting: true,
+    }
   ],
+  [
+    "junit",
+    {
+      outputDir: "./reports/junit",
+      outputFileFormat: function(options) {
+        return `results-${options.cid}.xml`
+      }
+    }
+  ]
+],
 
   // If you are using Cucumber you need to specify the location of your step definitions.
   cucumberOpts: {
